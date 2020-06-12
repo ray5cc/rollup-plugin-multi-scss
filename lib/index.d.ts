a72dbfb3bf0f1f00ff77bab0a97e88da46d775be
ray5cc <ray5cc@outlook.com>
2020-06-12 12:50:42 +0800

    delete lib file

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Plugin } from 'rollup';
-import RollupPluginMultiScssOptions from './RollupPluginMultiScssOptions';
-export { RollupPluginMultiScssOptions };
-export default function multiScss({ include, includePaths, exclude, prefix, processor, assetsPath, keepName, }?: RollupPluginMultiScssOptions): Plugin;
